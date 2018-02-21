4ea2550c5289c84421dcb80bce8875ca04f1ff3d
Nhan Ngọc Bình <nnbinh1987@outlook.com>
2018-02-21 22:59:47 +0700

    MongoDB and Mongoose

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -5,42 +5,42 @@ const router = express.Router();
 //Handle incomming request to /orders
 router.get('/', (req, res, next) => {
     res.status(200).json({
-        message : 'Handling GET request to /orders'
+        message: 'Handling GET request to /orders'
     });
 });
 
 router.post('/', (req, res, next) => {
     const order = {
-        productId : req.body.productId,
-        quantity : req.body.quantity
+        productId: req.body.productId,
+        quantity: req.body.quantity
     }
     res.status(201).json({
-        message : 'Order was created!',
-        createdOrder : order
+        message: 'Order was created!',
+        createdOrder: order
     });
 });
 
 router.get('/:orderId', (req, res, next) => {
     const id = req.params.orderId;
     res.status(200).json({
-        message : 'You get an Order ID',
-        id : id
+        message: 'You get an Order ID',
+        id: id
     });
 });
 
 router.patch('/:orderId', (req, res, next) => {
     const id = req.params.orderId;
     res.status(200).json({
-        message : 'Order patched',
-        id : id
+        message: 'Order patched',
+        id: id
     });
 });
 
 router.delete('/:orderId', (req, res, next) => {
     const id = req.params.orderId;
     res.status(200).json({
-        message : 'Order deleted',
-        id : id
+        message: 'Order deleted',
+        id: id
     });
 });
 
--- app.js
+++ app.js
@@ -2,21 +2,32 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+mongoose.connect(
+    'mongodb://nnbinh1987:' +
+    process.env.MONGO_ATLAS_PW +
+    '@node-rest-api-shard-00-00-nc4it.mongodb.net:27017,node-rest-api-shard-00-01-nc4it.mongodb.net:27017,node-rest-api-shard-00-02-nc4it.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-api-shard-0&authSource=admin',
+    {
+        //useMongoClient : true
+    }
+)
+
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
-        'Access-Control-Allow-Headers', 
+        'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
-    if (req.method === 'OPTIONS'){
+    if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH')
         return res.status(200).json({});
     }
@@ -37,8 +48,8 @@ app.use((req, res, next) => {
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
-        error : {
-            message : error.message
+        error: {
+            message: error.message
         }
     });
 });
